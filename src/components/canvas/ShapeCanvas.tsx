7f3d946eb791e8b94ac52b7e966998e1dca44fdf
Jack Frate <jack.frate24@gmail.com>
2023-01-17 20:33:24 -0600

    pointer coordinate system wokring

--- src/components/canvas/ShapeCanvas.tsx
+++ src/components/canvas/ShapeCanvas.tsx
@@ -1,31 +1,58 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { drawShape } from "../../types/ShapeStrategy";
 import { ShapeData } from "../../types/ShapeType";
+
+type Coordinate = { x: number; y: number };
+
+const checkIfPointerInShape = (
+    { x, y }: Coordinate,
+    shapePaths: Map<string, Path2D>,
+    ctx: CanvasRenderingContext2D
+): string | undefined => {
+    shapePaths.forEach((path, shapeId) => {
+        // Very nice that we can just cache the paths and use that to check
+        if (ctx.isPointInPath(path, x, y)) {
+            return shapeId;
+        }
+    });
+
+    return undefined;
+};
 
 type Props = {
     shapes: ShapeData[];
     hoveredShapeId: string | undefined;
     selectedShapeIds: string[];
-    onShapeHover: (shapeId: string | undefined) => void
+    onShapeHover: (shapeId: string | undefined) => void;
 };
 
 const ShapeCanvas: React.FC<Props> = ({
     shapes,
     hoveredShapeId,
     selectedShapeIds,
-    onShapeHover
+    onShapeHover,
 }: Props) => {
-    // I used RxJS a lot, as my last job was in Angular. It's really good for chaining
-    // UI events in non-messy ways
-
     // TODO: when doing mouse hover / clicking, search in reverse to get the frontmost element
     // TODO: try the double canvas approach when finished
+
+    const [pointerIsDown, setPointerIsDown] = useState(false);
+    const [pointerdownCoordinates, setPointerdownCoordinates] =
+        useState<Coordinate>({ x: 0, y: 0 });
+
+    const [shapePaths, setShapePaths] = useState<Map<string, Path2D>>(
+        new Map()
+    );
+
+    const [currentPointerCoordinates, setCurrentPointerCoordinates] =
+        useState<Coordinate>({ x: 0, y: 0 });
+
+    const containerRef = useRef<HTMLDivElement>(null);
 
     const shapeCanvasRef = useRef<HTMLCanvasElement>(null);
     // Used to avoid re-drawing entire canvas on hover
     const outlineCanvasRef = useRef<HTMLCanvasElement>(null);
 
-    // Update shapes and
+    // Update shapes and their selection outlines
     useEffect(() => {
         const shapeCanvas = shapeCanvasRef.current;
         if (!shapeCanvas) {
@@ -47,18 +74,26 @@ const ShapeCanvas: React.FC<Props> = ({
             selectedShapeIds.includes(id)
         );
 
+        const newPathMap = new Map<string, Path2D>();
+
         unSelectedShapes.forEach((shape) => {
-            drawShape(shape, shapeContext);
+            const path = drawShape(shape, shapeContext);
+            newPathMap.set(shape.id, path);
         });
 
         selectedShapes.forEach((shape) => {
-            drawShape(shape, shapeContext);
+            const path = drawShape(shape, shapeContext);
+            newPathMap.set(shape.id, path);
+
             // TODO: draw selected outline
         });
+
+        setShapePaths(newPathMap);
 
         console.log("RERENDER");
     }, [shapes, selectedShapeIds]);
 
+    // Update hover on shapes
     useEffect(() => {
         const outlineCanvas = outlineCanvasRef.current;
         if (
@@ -73,10 +108,36 @@ const ShapeCanvas: React.FC<Props> = ({
         console.log(`${hoveredShapeId} is hovered`);
     }, [shapes, hoveredShapeId]);
 
+    useEffect(
+        () => console.log(currentPointerCoordinates),
+        [currentPointerCoordinates]
+    );
+
     return (
         // In my experience, canvases can be weird with events
         // So I'll do all the event captures on this div
-        <div className="h-[500px] w-[500px] bg-white relative">
+        <div
+            className="h-[500px] w-[500px] bg-white relative"
+            ref={containerRef}
+            onMouseDown={(e) => {
+                setPointerIsDown(true);
+                setPointerdownCoordinates({
+                    x: e.nativeEvent.offsetX,
+                    y: e.nativeEvent.offsetY,
+                });
+            }}
+            onMouseUp={() => {
+                setPointerIsDown(false);
+            }}
+            // ensure we don't get buggy dragging
+            onMouseLeave={() => setPointerIsDown(false)}
+            onMouseMove={(e) =>
+                setCurrentPointerCoordinates({
+                    x: e.nativeEvent.offsetX,
+                    y: e.nativeEvent.offsetY,
+                })
+            }
+        >
             <canvas
                 className="absolute z-0 pointer-events-none"
                 width={500}
